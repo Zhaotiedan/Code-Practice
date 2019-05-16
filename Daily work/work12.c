f627fb6fc55d2aa82ae4563ddd3676852827d424
Zhaotiedan <641691302@qq.com>
2019-05-16 23:53:16 +0800

    change work12.c

--- Daily work/work12.c
+++ Daily work/work12.c
@@ -1,6 +1,6 @@
-1.一个数组中只有两个数字是出现一次， 
+/*1.一个数组中只有两个数字是出现一次， 
 其他所有数字都出现了两次。 
-找出这两个只出现一次的数字，编程实现。 
+找出这两个只出现一次的数字，编程实现。 */
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -10,22 +10,21 @@ void Find_two_number(int *arr, int len)
 	int tmp = 0;
 	int pos = 0;
 	int a,b=0;
-	/*int	b = 0;*/
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; i++)//拿到异或结果
 	{
 		tmp = tmp ^ arr[i];
 	}
-	for (j = 0; j < 32; j++)
+	for (j = 0; j < 32; j++)//找到异或结果的1出现的位置j
 	{
 		if ((tmp >> j) & 1 == 1)
 		{
-			pos = j;
+			pos = j;//将结果保存在pos里面
 			break;
 		}
 	}
 	for (k = 0; k < len; k++)
 	{
-		if ((arr[k] >> pos) & 1 == 1)
+		if ((arr[k] >> pos) & 1 == 1)//按第一次出现1的位置是否为pos进行分组
 		{
 			a = a ^ arr[k];
 		}
@@ -42,9 +41,8 @@ int main()
 	system("pause");
 	return 0;
 }
-2.喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水， 
-给20元，可以多少汽水。 
-编程实现。 
+/*2.喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水， 
+给20元，可以多少汽水。 编程实现。*/ 
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -70,3 +68,4 @@ int main()
 	system("pause");
 	return 0;
 }
+//最终公式：total=2*money+1 可快速计算
