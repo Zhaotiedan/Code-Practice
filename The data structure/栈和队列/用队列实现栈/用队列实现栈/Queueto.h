952d19e80adde90a032e03df5e6316ffa49cc768
Zhaotiedan <641691302@qq.com>
2019-07-27 23:38:31 +0800

    perfect the code

---
+++ The data structure/栈和队列/用队列实现栈/用队列实现栈/Queueto.h
@@ -0,0 +1,9 @@
+#ifndef _QUEUETO_H_
+#define _QUEUETO_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#endif // !_QUEUETO_H_
+
---
+++ The data structure/栈和队列/用队列实现栈/用队列实现栈/main.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Queueto.h"
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
