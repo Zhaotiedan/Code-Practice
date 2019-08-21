87786671a9734fb5f637d64ab8112559a9822106
Zhaotiedan <641691302@qq.com>
2019-08-21 23:25:02 +0800

    update work16.c

--- C-code/Daily work/work16.c
+++ C-code/Daily work/work16.c
@@ -11,7 +11,7 @@ char *myString()
 int main()
 {
 	char * buffer = myString();
-	printf("%s\n", buffer);
+	printf("%s\n", buffer);//结果为烫烫烫的随机值，因为buffer为局部数组，函数结束后会被收回
 	system("pause");
 	return 0;
 }
