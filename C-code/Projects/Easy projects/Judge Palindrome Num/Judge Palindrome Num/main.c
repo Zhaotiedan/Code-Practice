2ce5351378044e15561be8f695aeb399afd7ca81
Zhaotiedan <641691302@qq.com>
2019-09-15 22:53:07 +0800

    add judge paliodrome number

--- C-code/Projects/Easy projects/Judge Palindrome Num/Judge Palindrome Num/main.c
+++ C-code/Projects/Easy projects/Judge Palindrome Num/Judge Palindrome Num/main.c
@@ -5,7 +5,22 @@
 
 int main()
 {
-	printf("hell\n");
+	int new = 0;
+	int num = 0;
+	scanf("%d", &num);
+	while (num)
+	{
+		new = new * 10 + num % 10;
+		num /= 10;
+	}
+	if (num == new)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
 	system("pause");
 	return 0;
 }
