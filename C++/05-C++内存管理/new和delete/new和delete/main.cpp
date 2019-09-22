1739d8bbd4364ba9eb0cd4f00179409f33571ee6
Zhaotiedan <641691302@qq.com>
2019-09-23 00:51:57 +0800

    update new and delete

--- C++/05-C++内存管理/new和delete/new和delete/main.cpp
+++ C++/05-C++内存管理/new和delete/new和delete/main.cpp
@@ -32,7 +32,7 @@ void Test2()
 }
 
 //3.new/delete操作自定义类型
-//new会调用构造函数 delete会调用析构函数，而malloc和free不会
+//new会调用构造函数 delete会调用析构函数，清理对象中的资源，而malloc和free不会
 class Test
 {
 public:
@@ -60,27 +60,54 @@ void Test3()
 	delete[] p2;
 
 	Test* p3 = (Test*)malloc(sizeof(Test)); //申请单个Test类型的空间
+	//malloc申请空间时不会调用构造函数--申请的是与对象大小相同的一块内存空间，不能将该块内存空间看成一个对象
 	free(p3);
 	Test* p4 = (Test*)malloc(sizeof(Test) * 10);//申请10个Test类型的空间
 	free(p4);
 }
 
 //4.如果new和delete没有匹配使用，产生后果?
-//
+//只要涉及到[]，必然会崩溃
 void Test4()
 {
 	Test* p1 = new Test;
-	free(p1);
+	free(p1);//不报错
 
 	Test* p2 = new Test[10];
 	delete p2;//报错
+
+	Test* p3 = new Test;
+	delete[] p3;//报错 崩溃
 }
+
+//5. new--->能够将申请空间的信息打印 delete--->能够将释放空间信息打印
+void* operator new(size_t size, const char* filename, const char* funName, size_t lineNo)
+{
+	cout << filename << "-" << funName << "-" << lineNo << "-" << size << endl;
+	return malloc(size);
+}
+
+void operator delete(void* p, const char* filename, const char* funName, size_t lineNo)
+{
+	cout << filename << "-" << funName << "-" << lineNo << endl;
+	free(p);
+}
+
+#define new new(__FILE__, __FUNCDNAME__, __LINE__)
+void Test5()
+{
+	int* p = new int;
+	delete p;
+}
+
+
 int main()
 {
 	Test1();
 	Test2();
 	Test3();
-	Test4();
+	//Test4();
+	Test5();
 
 	system("pause");
 	return 0;
