cab72e882bbeac18d5994575c45c7b387e85ecc6
Zhaotiedan <641691302@qq.com>
2019-09-25 23:26:49 +0800

    update string类

--- C++/07-STL/String类/String类/main.cpp
+++ C++/07-STL/String类/String类/main.cpp
@@ -22,7 +22,7 @@ void Test2()
 {
 	string s("change world");
 	//2.1求字符串有效字符长度和空间总大小
-	cout << s.size() << endl;
+	cout << s.size() << endl;//size()与length()实现完全相同，size()用的更多
 	cout << s.length() << endl;
 	cout << s.capacity() << endl;
 
@@ -70,8 +70,9 @@ void Test2()
 	cout << s1.size() << endl;//6
 	cout << s1.capacity() << endl;//31
 
-	//总结： 1. 如果是将string类中的有效元素缩小，只改变有效元素个数，不会改变底层空间大小
-//       2. 如果是将string类中的有效元素增多，可能需要扩容
+	/*总结：1. 如果是将string类中的有效元素缩小，只改变有效元素个数，不会改变底层空间大小
+            2. 如果是将string类中的有效元素增多，可能需要扩容 */
+
 
 	//2.5void reserve(size_t newcapacity): 扩容
 	string s2(20, '*');
@@ -94,15 +95,126 @@ void Test2()
 	cout << s2.size() << endl;//20
 	cout << s2.capacity() << endl;//47
 
-	//总结： 
-//newcapacity > oldcapacity(string类旧空间大小)：空间增多--->容量改变(最终容量大小 >= newCapacity)
-// newcapacity < oldcapacity(string类旧空间大小)：空间缩小，该函数直接返回 除非newcapacity < 15
-//只改变容量大小，不会改变有效元素个数
+  //总结： 
+//newcapacity > oldcapacity(string类旧空间大小)：空间增多--->容量改变(最终容量大小 >= newCapacity，vs下大约以1.5的倍率增长)
+// newcapacity < oldcapacity(string类旧空间大小)：空间缩小，不影响容量
+//reserve最多只改变容量大小，不会改变有效元素个数
 }
+
+//3. 利用reserve提高插入数据的效率，避免增容带来的开销
+//未加reserve之前：
+void TestPushBack()
+{
+	string s;
+	size_t sz = s.capacity();
+	cout << "making s grow:\n";
+	for (int i = 0; i < 100; ++i)
+	{
+		s.push_back('c');//不停地尾插，自动扩容
+		if (sz != s.capacity())
+		{
+			sz = s.capacity();
+			cout << "capacity changed: " << sz << '\n';
+		}
+	}
+}
+//加reserve之后：
+//因为已经预留了100的空间，所以不会增容了,sz会恒等于capacity。但如果一旦后面需要空间超过100，就会不停地增容
+void TestPushBackReserve()
+{
+	string s;
+	s.reserve(100);
+	size_t sz = s.capacity();
+	cout << "making s grow:\n";
+	for (int i = 0; i < 100; i++)
+	{
+		s.push_back('c');
+		if (sz != s.capacity())
+		{
+			sz = s.capacity();
+			cout << "Capacity changed:" << sz << '\n' << endl;
+		}
+	}
+}
+
+//4.string类对象的访问及遍历
+
+//4.1 访问方式
+//4.1.1 operator[] 和at()返回pos位置的字符，const string类对象调用
+void Test3()
+{
+	string s1("I love");
+	const string s2("my country");
+	cout << s1 << " " << s2 << endl;
+	cout << s1[0] << " " << s2[0] << endl;
+
+	s1[0] = 'H';
+	s1.at(1) = 'h';
+	cout << s1 << endl;
+	//s2[0]='H'  报错 对象s2被const修饰，不能被修改
+
+	//关于数组越界
+	/*cout << s1[10] << endl;   数组越界，assert触发
+	cout << s1.at(10) << endl;抛出out_of_range异常*/
+}
+//4.1.2 begin+ end begin获取一个字符的迭代器 + end获取最后一个字符下一个位置的迭
+//4.1.3 rbegin + rend
+
+//4.2 遍历方式：
+void Test4()
+{
+	string s("I am the best");
+	//1.for+operator[]
+	for (size_t i = 0; i < s.size(); i++)
+	{
+		cout << s[i];
+	}
+	cout << '\n';
+	//2.迭代器
+	string::iterator it = s.begin();
+	while (it != s.end())
+	{
+		cout << *it;
+		it++;
+	}
+	cout << '\n';
+	//3.范围for
+	for (auto e : s)
+	{
+		cout << e;
+	}
+	cout << '\n';
+}
+
+//5.string类对象的修改操作
+void Test5()
+{
+	string s;
+	s.push_back('z');//在s后面插入一个字符z
+	s.append("ss");//在s后面追加一个字符ss
+	s += 'n';//在s后面追加一个字符n
+	s += "b!";//在s后面加一个字符串"b！"
+	cout << s << endl;
+
+	string s2("I love");
+	string s3(" pxy");
+	s2 += s3;
+	s2.append(3,'!');
+	cout << s2 << endl;
+	cout << s2.c_str() << endl;//以C语言的方式打印字符串
+
+}
+
 int main()
 {
 	Test1();
 	Test2();
+	TestPushBack();
+	TestPushBackReserve();
+	Test3();
+	Test4();
+	Test5();
+
 	system("pause");
 	return 0;
 }
