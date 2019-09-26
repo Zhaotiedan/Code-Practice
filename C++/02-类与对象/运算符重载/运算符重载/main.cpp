f0f4a2721d19d9f130f0a9e701784adbf816b319
Zhaotiedan <641691302@qq.com>
2019-09-27 00:27:53 +0800

    update operator

--- C++/02-类与对象/运算符重载/运算符重载/main.cpp
+++ C++/02-类与对象/运算符重载/运算符重载/main.cpp
@@ -12,9 +12,9 @@ public:
 		_month = month;
 		_day = day;
 	}
-	bool operator==(const Date& d1, const Date& d2)
+	bool operator==(const Date& d2)//，左操作数是this指向的调用函数的对象
 	{
-		return d1._year == d2.
+		return _year == d2._year&&_month == d2._month&&_day == d2._day;
 	}
 private:
 	int _year;
@@ -25,7 +25,7 @@ int main()
 {
 	Date d1(2018, 9, 26);
 	Date d2(2018, 9, 27);
-
+	cout << (d1 == d2) << endl;
 	system("pause");
 	return 0;
 }
