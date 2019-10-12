2b3aa6c668e880733d82c158bbba033b39f20b6c
Zhaotiedan <641691302@qq.com>
2019-10-13 00:00:03 +0800

    update the realiztion of vector

--- C++/07-STL/vector模拟实现/vector模拟实现/main.cpp
+++ C++/07-STL/vector模拟实现/vector模拟实现/main.cpp
@@ -3,6 +3,35 @@
 #include<iostream>
 using namespace std;
 
+
+template<class T>
+
+class vector
+{
+public:
+	//构造
+	vector()
+		:_start(nullptr)
+		,_finish(nullptr)
+		,_endofstorage(nullptr)
+	{}
+
+	vector(int n, const T& data)
+		:_start(new T[n])
+	{
+		for (size_t i = 0; i < n; i++)
+		{
+			_start[i] = data;
+		}
+		_finish = _start + n;
+		_endofstorage = _finish;
+	}
+	//销毁
+private:
+	T* _start;//起始指针
+	T* _finish;//终止指针  求size
+	T* _endofstorage;//求capacity
+};
 int main()
 {
 	system("pause");
