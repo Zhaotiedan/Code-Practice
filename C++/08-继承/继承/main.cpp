28d772a97ca78f69f03f1cbb099ca3268333cc81
Zhaotiedan <641691302@qq.com>
2019-11-02 23:56:53 +0800

    add 08--Inheritance

--- C++/08-继承/继承/main.cpp
+++ C++/08-继承/继承/main.cpp
@@ -290,7 +290,7 @@ void Test4()
 	cout << d._pub << endl;
 }
 
-/*4.基类和派生类对象赋值转换*/
+/*4.基类和派生类对象赋值转换   前提：public继承*/ 
 //4.1 派生类对象 可以赋值给 基类的对象 + 基类的指针 + 基类的引用。
 //4.2 基类对象不能赋值给派生类
 //4.3 基类的指针可以通过强制类型转换赋值给派生类的指针
@@ -345,9 +345,12 @@ void Test5()
 
 /*5.继承中的作用域*/
 //1. 在继承体系中基类和派生类都有独立的作用域。所以同名函数就不会重载
-//2. 子类和父类中有同名成员，子类成员将屏蔽父类对同名成员的直接访问，这种情况叫隐藏，也叫重定义。（在子类成员函数中，可以使用 基类::基类成员 显示访问）
-//3. 需要注意的是如果是成员函数的隐藏，只需要函数名相同就构成隐藏。
-//4. 注意在实际中在继承体系里面最好不要定义同名的成员。
+//2.同名隐藏：派生类和基类中具有相同名称的成员（成员函数和成员变量）
+//3.如果使用派生类对象直接访问同名成员，则派生类会优先访问自己的成员，相当于基类的同名成员隐藏
+//注意：成员变量同名---->与成员变量类型是否相同无关
+//      成员函数同名---->与函数原型是否相同无关
+//想要 通过派生类访问基类成员的方法：   使用时，在同名前面+基类的名称和作用域限定符
+
 
 //同名隐藏
 //基类
@@ -387,6 +390,228 @@ void Test6()
 
 /*6.4.派生类的默认成员函数*/
 
+//1. 派生类的构造函数会调用基类的构造函数初始化基类的那一部分成员。
+
+//1.1如果基类的构造函数是无参或者全缺省的默认构造函数，则派生类初始化列表的位置调用或不调用都可以，如果用户没有调用，则编译器会默认调用
+//1.2如果基类没有默认的构造函数(基类自定义了构造函数)，则必须在派生类构造函数的初始化列表阶段显示调用该显式构造函数
+//因为显式构造函数后，编译器就找不到默认构造函数了
+
+//验证1.1
+class Base7
+{
+public:
+	////无参
+	//Base7()
+	//	:_b(10)
+	//	
+	//{
+	//	cout << "无参Base7()" << endl;
+	//}
+
+	//全缺省
+	Base7(int b=100)
+		:_b(b)
+	{
+		cout << "全缺省Base7(int b=100)" << endl;
+	}
+//protected:
+public:
+	int _b;
+};
+
+class Derived7 : public Base7
+{
+public:
+	Derived7()
+		:_d(10)
+	{
+		cout << "无参Derived7()" << endl;
+	}	
+//protected:
+public:
+	int _d;
+};
+
+void Test7()
+{
+	Base7 b;//调一次构造
+	Derived7 d;//调一次基类构造，调一此自身构造
+	cout << b._b << endl;
+	cout << d._d << endl;
+	cout << d._b << endl;
+	cout << endl;
+}
+//验证1.2
+class Base8
+{
+public:
+	Base8(int b)
+		:_b(b)
+	{
+		cout << "自定义Base8()" << endl;
+	}
+//protected:
+public:
+	int _b;
+};
+
+class Derived8 : public Base8
+{
+public:
+	Derived8()
+		:Base8(100)
+		,_d(10)
+	{
+		cout << "无参Derived8()" << endl;
+	}
+//protected:
+public:
+	int _d;
+};
+
+void Test8()
+{
+	Base8 b(1000);
+	Derived8 d;
+	cout << b._b << endl;
+	cout << d._d << endl;
+	cout << endl;
+}
+
+//2.派生类的拷贝构造函数会调用基类的拷贝构造完成基类的拷贝初始化。
+
+//2.1基类如果没有显示定义构造函数，则派生类也可以不用定义，即两个类都可以采用默认构造函数，前提：不涉及资源管理
+//2.2基类如果显式定义了构造函数，则派生类就必须在里面初始化列表显式调用基类的构造函数
+
+
+//3.赋值运算符重载：
+//3.1如果基类没有显示定义派生类也可以不提供，除非派生类需要作出其他操作，再根据情况给出
+//3.2如果基类显式给出了赋值运算符重载，一般派生类也要提供，而且要在其中调用基类的重载
+
+//验证2.1
+class Base9
+{
+public:
+	Base9(int b)
+		:_b(b)
+	{}
+	//protected:
+public:
+	int _b;
+};
+class Derived9 :public Base9
+{
+public:
+	Derived9(int d)
+		:Base9(100)
+		,_d(d)
+	{}
+
+public:
+	int _d;
+};
+
+void Test9()
+{
+	Derived9 d1(1000);
+	Derived9 d2(d1);
+	cout << d1._d << endl;//1000
+	cout << d1._b << endl;//100
+	cout << d2._d << endl;//1000
+	cout << d2._b << endl;//100
+	cout << endl;
+}
+
+//验证2.2  3.2
+class Base10
+{
+public:
+	Base10(int b)
+		: _b(b)
+	{}
+
+	Base10(const Base10& b)
+		: _b(b._b)
+	{}
+
+	Base10& operator=(const Base10& b)
+	{
+		if (this != &b)
+		{
+			_b = b._b;
+		}
+
+		return *this;
+	}
+public:
+	int _b;
+};
+
+class Derived10 : public Base10
+{
+public:
+	Derived10(int b, int d)
+		: Base10(b)
+		, _d(d)
+	{}
+
+	Derived10(const Derived10& d)
+		: Base10(d)
+		, _d(d._d)
+	{}
+
+	Derived10& operator=(const Derived10& d)
+	{
+		if (this != &d)
+		{
+
+			//*this = d;
+			// 给基类部分成员赋值
+			Base10::operator=(d);
+			// 给派生类新增加成员赋值
+			_d = d._d;
+		}
+		return *this;
+	}
+public:
+	int _d;
+};
+
+void Test10()
+{
+	Derived10 d1(1, 2);
+	Derived10 d2(d1);
+
+	Derived10 d3(3, 4);
+	d2 = d3;
+	cout << d1._b << endl;
+	cout << d1._d << endl;
+	cout << d2._b << endl;
+	cout << d2._d << endl;
+	cout << d3._b << endl;
+	cout << d3._d << endl;
+}
+
+//3.派生类和基类构造函数和析构函数的调用次序
+class Base10
+{
+public:
+	Base10(int b)
+		:_b(b)
+	{
+		cout << "Base10::Base10(int)" << endl;
+	}
+	~Base10()
+	{
+		cout << "Base10::~Base10()" << endl;
+	}
+public:
+	int _b;
+};
+class Derived10
+{
+
+};
 int main()
 {
 	Test();
@@ -396,6 +621,10 @@ int main()
 	Test4();
 	Test5();
 	Test6();
+	Test7();
+	Test8();
+	Test9();
+	Test10();
 
 	system("pause");
 	return 0;
