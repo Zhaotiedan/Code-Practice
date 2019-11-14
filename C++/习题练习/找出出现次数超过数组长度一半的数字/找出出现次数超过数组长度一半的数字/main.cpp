02a7bf72c6c28cb74cb4e1fafb35373d9017a394
Zhaotiedan <641691302@qq.com>
2019-11-14 13:59:34 +0800

    debug

--- C++/习题练习/找出出现次数超过数组长度一半的数字/找出出现次数超过数组长度一半的数字/main.cpp
+++ C++/习题练习/找出出现次数超过数组长度一半的数字/找出出现次数超过数组长度一半的数字/main.cpp
@@ -1,5 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+/*链接：https://www.nowcoder.com/questionTerminal/e8a1b01a2df14cb2b228b30ee6a92163
+来源：牛客网
+
+数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+*/
+
 #include<iostream>
 using namespace std;
 #include<vector>
@@ -9,7 +16,7 @@ class Solution {
 public:
 	int MoreThanHalfNum_Solution(vector<int> numbers) {
 		int midlength = numbers.size() / 2;
-		vector<int> count(numbers.size());
+		vector<int> count(numbers.size()*2);
 		for (int i = 0; i < numbers.size(); i++)
 		{
 			count[numbers[i]]++;
@@ -26,7 +33,9 @@ public:
 };
 int main()
 {
-	vector<int> v{ 1,2,3,2,2,2,5,4,2,1,1,1,1,1,1,1,1,1,1,1 };
+	//vector<int> v{ 1,2,3,2,2,2,5,4,2,1,1,1,1,1,1,1,1,1,1,1 };
+	vector<int> v{ 1 };
+
 	Solution s;
 	int ret=s.MoreThanHalfNum_Solution(v);
 	cout << ret << endl;
