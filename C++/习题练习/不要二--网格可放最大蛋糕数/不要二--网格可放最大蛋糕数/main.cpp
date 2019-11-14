31baea62a4b6819056758166003ef458e3793cd6
Zhaotiedan <641691302@qq.com>
2019-11-14 14:43:27 +0800

    add new practice

--- C++/习题练习/不要二--网格可放最大蛋糕数/不要二--网格可放最大蛋糕数/main.cpp
+++ C++/习题练习/不要二--网格可放最大蛋糕数/不要二--网格可放最大蛋糕数/main.cpp
@@ -1,5 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+/*链接：https://www.nowcoder.com/questionTerminal/1183548cd48446b38da501e58d5944eb
+来源：牛客网
+
+二货小易有一个W*H的网格盒子，网格的行编号为0~H-1，网格的列编号为0~W-1。每个格子至多可以放一块蛋糕，任意两块蛋糕的欧几里得距离不能等于2。
+对于两个格子坐标(x1,y1),(x2,y2)的欧几里得距离为:
+( (x1-x2) * (x1-x2) + (y1-y2) * (y1-y2) ) 的算术平方根
+小易想知道最多可以放多少块蛋糕在网格盒子里。*/
 #include<iostream>
 using namespace std;
 
--- C++/习题练习/求子数组的连续最大和/求子数组的连续最大和/main.cpp
+++ C++/习题练习/求子数组的连续最大和/求子数组的连续最大和/main.cpp
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+/*一个数组有 N 个元素，求连续子数组的最大和。 例如：[-1,2,1]，和最大的连续子数组为[2,1]，其和为 3*/
 #include<iostream>
 using namespace std;
 #include<vector>
