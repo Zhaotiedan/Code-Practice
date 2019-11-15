1f956b2ae66df92493537bbd7f571e1e5eb4372d
Zhaotiedan <641691302@qq.com>
2019-11-15 19:08:45 +0800

    add a new solution

--- C++/习题练习/n乘m棋盘走法/n乘m棋盘走法/main.cpp
+++ C++/习题练习/n乘m棋盘走法/n乘m棋盘走法/main.cpp
@@ -7,6 +7,8 @@
 #include<iostream>
 using namespace std;
 
+
+//µÝ¹é
 int GetFun(int n, int m)
 {
 	if (n == 0 || m == 0)
@@ -26,13 +28,42 @@ int GetFun(int n, int m)
 		return GetFun(n - 1, m) + GetFun(n, m - 1);
 	}
 }
+
+//Ñ­»·
+int GetFun1(int n, int m)
+{
+	int count = 0;
+	int tempn = n;
+	int tempm = m;
+	while (tempn)
+	{
+		for (int i = 1; i <= n + 1; i++)
+		{}
+		tempn--;
+		for (int j = 1; j <= m + 1; j++)
+		{}
+		count++;
+	}
+	while (tempm)
+	{
+		for (int j = 1; j <= m + 1; j++)
+		{
+		}
+		tempm--;
+		for (int i = 1; i <= n + 1; i++)
+		{
+		}
+		count++;
+	}
+	return count;
+}
 int main()
 {
 	int n = 0;
 	int m = 0;
 	while (cin >> n >> m)
 	{
-		cout << GetFun(n, m) << endl;
+		cout << GetFun1(n, m) << endl;
 	}
 	system("pause");
 	return 0;
