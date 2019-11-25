91ddb64f0a8fb2c09a5102c1f9f981a1046be31a
Zhaotiedan <641691302@qq.com>
2019-11-25 21:29:49 +0800

    add rabbit question

---
+++ C++/习题练习/统计每个月所生兔子总数/统计每个月所生兔子总数/main.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+
+int GetNum(int month)
+{
+	int sum = 0;
+
+}
+int main()
+{
+	int month = 0;
+	cin >> month;
+	system("pause");
+	return 0;
+}
