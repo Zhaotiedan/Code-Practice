fd04ff8215be4a851690651d677fde75e997d2e6
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2020-01-29 23:49:47 +0800

    Update work18.c

--- C-code/Daily work/work18.c
+++ C-code/Daily work/work18.c
@@ -39,7 +39,42 @@ public:
 示例:
 输入: "Hello World"
 输出: 5
-	
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        if(s.size()==0)
+        {
+            return 0;
+        }
+        int num = s.size();
+        while ((s[num - 1] == ' ') && (num >= 0)) {
+            num--;
+            }
+        string str = s.substr(0,num);
+        size_t pos=str.rfind(' ');
+        int length=0;
+        if(pos!=s.npos)
+        {
+            for(int i=pos;i<str.size()-1;i++)
+            {
+                length++;
+            }
+            return length;
+        }
+        else
+        {
+            return num;
+        }
+    }
+};	
+
+//3.leetcode88
+给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+说明:
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素
+
 #include<algorithm>
 class Solution {
 public:
@@ -59,3 +94,4 @@ public:
         }
     }
 };
+
