7c638e996329ab590c65397ccd261d16e7e2e3c4
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2020-01-29 23:08:05 +0800

    Update work18.c

--- C-code/Daily work/work18.c
+++ C-code/Daily work/work18.c
@@ -40,4 +40,22 @@ public:
 输入: "Hello World"
 输出: 5
 	
-
+#include<algorithm>
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(nums1.size()>=m+n)
+        {
+            nums1.erase(nums1.begin()+m,nums1.end());
+            for(int i=0;i<nums2.size();i++)
+            {
+                nums1.push_back(nums2[i]);
+            }
+            sort(nums1.begin(),nums1.end());
+        }
+        else
+        {
+            return;
+        }
+    }
+};
