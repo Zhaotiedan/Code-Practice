d98af47d59a67fead4331fa8a0ee7cebd3a02fa9
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2020-02-02 20:51:35 +0800

    Update work18.c

--- C-code/Daily work/work18.c
+++ C-code/Daily work/work18.c
@@ -95,11 +95,11 @@ public:
     }
 };
 
-//leetcode 127
+//4.leetcode 127
 给定一个整数数组，判断是否存在重复元素。
 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
 
-//1.个人暴力解法 可能超时
+//个人暴力解法 可能超时
 #include <algorithm>
 class Solution {
 public:
@@ -121,7 +121,7 @@ public:
     }
 };
 
-//2.将nums进行排序然后判断相邻是否有相同元素
+//将nums进行排序然后判断相邻是否有相同元素
 class Solution {
     static bool cmp(const int &a,const int &b)
     {
@@ -139,3 +139,23 @@ public:
         return false;
     }
 };
+
+//leetcode 925
+你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。
+你将会检查键盘输入的字符 typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么就返回 True。
+示例 1：
+输入：name = "alex", typed = "aaleex"
+输出：true
+解释：'alex' 中的 'a' 和 'e' 被长按。
+示例 2：
+输入：name = "saeed", typed = "ssaaedd"
+输出：false
+解释：'e' 一定需要被键入两次，但在 typed 的输出中不是这样。
+示例 3：
+输入：name = "leelee", typed = "lleeelee"
+输出：true
+示例 4：
+输入：name = "laiden", typed = "laiden"
+输出：true
+解释：长按名字中的字符并不是必要的。
+
