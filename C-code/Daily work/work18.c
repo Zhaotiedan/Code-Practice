56749fdac13deaac1b44ecc101d3d71a8e07cc97
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2020-02-04 17:34:24 +0800

    Update work18.c

--- C-code/Daily work/work18.c
+++ C-code/Daily work/work18.c
@@ -140,7 +140,7 @@ public:
     }
 };
 
-//leetcode 925
+//3.leetcode 925
 你的朋友正在使用键盘输入他的名字 name。偶尔，在键入字符 c 时，按键可能会被长按，而字符可能被输入 1 次或多次。
 你将会检查键盘输入的字符 typed。如果它对应的可能是你的朋友的名字（其中一些字符可能被长按），那么就返回 True。
 示例 1：
@@ -187,4 +187,25 @@ public:
 		}
 		return true;
 	}
+};
+
+//6.leetcode 977
+给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。
+
+示例 2：
+输入：[-7,-3,2,3,11]
+输出：[4,9,9,49,121]
+
+#include<algorithm>
+#include<math.h>
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        for(int i=0;i<A.size();i++)
+        {
+            A[i]=pow(abs(A[i]),2);
+        }
+        sort(A.begin(),A.end());
+        return A;
+    }
 };
