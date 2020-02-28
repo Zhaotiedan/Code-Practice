3c611f1234262056ea0fc622cfeeef2420f0177e
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2020-02-28 23:20:10 +0800

    Update main.cpp

--- C++/11-智能指针/11-智能指针/main.cpp
+++ C++/11-智能指针/11-智能指针/main.cpp
@@ -1,4 +1,4 @@
-https://baijiahao.baidu.com/s?id=1642457254477877779&wfr=spider&for=pc
+参考博文：https://baijiahao.baidu.com/s?id=1642457254477877779&wfr=spider&for=pc
 
 #define _CRT_SECURE_NO_WARNINGS 1
 
