afa616e367b3008648e60b6f94475bd71a168f8a
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2020-04-17 23:15:51 +0800

    update RBtree

--- C++/12-树形结构的关联式容器/红黑树/main.cpp
+++ C++/12-树形结构的关联式容器/红黑树/main.cpp
@@ -1,11 +1,9 @@
-#include<iostream>
-#include"RBtree.hpp"
-
-using namespace std;
+//红黑树为什么可以保证最长路径节点不会超过最短路径节点的两倍：全黑情况
+#include"RBtree.h"
 
 int main()
 {
-
+	TestRBtree();
 	system("pause");
 	return 0;
 }
