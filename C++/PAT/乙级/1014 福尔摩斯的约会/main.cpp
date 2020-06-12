792158e4b0adf36a5e592f0a3b37eb1a2eb91174
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2020-06-12 23:19:59 +0800

    finish 1014 the date

--- C++/PAT/乙级/1014 福尔摩斯的约会/main.cpp
+++ C++/PAT/乙级/1014 福尔摩斯的约会/main.cpp
@@ -21,3 +21,103 @@ d&Hyscvnm
 输出样例：
 
 THU 14:04
+  
+  //本代码牛客测试用例全过，pat（16/20）
+
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<string>
+#include<cmath>
+
+int main()
+{
+    string s1,s2,s3,s4;
+    cin>>s1>>s2>>s3>>s4;
+    int p1=0;//s1和s2第一个相同大写字母下标
+    int p2=0;//s1和s2第二个相同的大写字母下标
+    int p3=0;//s3和s4第一对相同的英文字母出现的位置
+    int temp=0;
+    
+    int flag=1;//标志一下s1和s2是当前的第几个相同的字母
+    
+    string v[7]={"MON","TUE","WED","THU","FRI","SAT","SUN"};
+    //搜索s1
+    for(int i=0;i<s1.size();i++)
+    {
+        if((s1[i]>=65&&s1[i]<=71)&&(flag==1))//星期的情况 在A~G之间  
+        {
+            //搜索s2
+            for(int j=0;j<s2.size();j++)
+            {
+                //找到了 第一个
+                if(s2[j]==s1[i]&&(i==j)&&(flag==1))
+                {
+                    flag=2;
+                    p1=s2[j]-65;//在v中的下标
+                    temp=j+1;//
+                    i++;
+                    cout<<v[p1]<<" ";
+                    //cout<<temp;
+                }
+            }
+        }
+        else if((flag==2) && ((s1[i]>=48&&s1[i]<=57) || (s1[i]>=65&&s1[i]<=78)))//钟头的情况,已经输出了星期
+        {
+            //数字0~9
+            if(s1[i]>=48&&s1[i]<=57)
+            {
+                for(int k=temp;k<s2.size();k++)
+                {
+                    
+                    if((s2[k]==s1[i])&&(k==i))
+                    {
+                        cout<<"0"<<s1[i]<<":";
+                        flag=3;
+                        break;
+                    }
+                }
+                
+            }
+            //字母
+            else if(s1[i]>=65&&s1[i]<=78)
+            {
+                
+                for(int t=temp;t<s2.size();t++)
+                {
+                    if((s2[t]==s1[i])&&(t==i))
+                    {
+                        p2=s2[t]-65;
+                        cout<<p2+10<<":";
+                        flag=3;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    
+    //搜索s3
+    for(int i=0;i<s3.size();i++)
+    {
+        //只要是英文字母
+        if( (s3[i]>=65&&s3[i]<=90) || (s3[i]>=97&&s3[i]<=122) )
+        {
+            if((s4[i]==s3[i])&&(flag==3))
+            {
+                p3=i;
+                if(p3<10)
+                {
+                    cout<<"0"<<p3;
+                    flag=4;
+                }
+                else
+                {
+                    cout<<p3;
+                    flag=4;
+                }
+            }
+        }
+    }
+    return 0;
+}
