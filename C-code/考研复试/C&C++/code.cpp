f0ef1985b9ed1b16a152c1332c57c214eaa40709
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2022-02-26 22:25:59 +0800

    Create code.cpp

---
+++ C-code/考研复试/C&C++/code.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int  main()
+{
+	//printf("%d",0);
+
+	//char x = 0xFFFF;
+	//printf("%d\n", x--);
+
+	/*char c = 'A';
+	if ('0' <= c <= '9') printf("YES");
+	else printf("NO");*/
+
+	int a = 10;
+	printf("%d\n", a);
+	printf("%6d\n", a);
+
+	system("pause");
+	return 0;
+}
+
