f31acceff4db54816a5f162bf7460e0989d1fd28
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2022-02-27 23:39:57 +0800

    Update code.cpp

--- C-code/考研复试/C&C++/code.cpp
+++ C-code/考研复试/C&C++/code.cpp
@@ -15,9 +15,20 @@ int  main()
 	if ('0' <= c <= '9') printf("YES");
 	else printf("NO");*/
 
+	/*
 	int a = 10;
 	printf("%d\n", a);
-	printf("%6d\n", a);
+	printf("%6d\n", a);*/
+
+	/*double a = 0;
+	float b = 0;
+	printf("%6.2f\n", a);
+	printf("%lf\n", b);*/
+
+	long long a = 123456789;
+	printf("%d\n", a);
+	printf("%ld", a);
+
 
 	system("pause");
 	return 0;
