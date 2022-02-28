cf8cc7b11427a0c6e24c74a3e5d7c40c498380ac
Zhaotiedan <48014011+Zhaotiedan@users.noreply.github.com>
2022-02-28 21:54:17 +0800

    Update code.cpp

--- C-code/考研复试/C&C++/code.cpp
+++ C-code/考研复试/C&C++/code.cpp
@@ -25,10 +25,23 @@ int  main()
 	printf("%6.2f\n", a);
 	printf("%lf\n", b);*/
 
-	long long a = 123456789;
+	/*long long a = 123456789;
 	printf("%d\n", a);
-	printf("%ld", a);
+	printf("%ld", a);*/
 
+	//int x = 8, y = -7, z = 9;
+	//if (x < y)
+	//	if (y < 0)
+	//		z = 0;
+	//	else//与最近的if配套
+	//		z = -1;
+	//printf("%d\n", z);
+
+	int i, j, m = 0;
+	for (i = 1; i <= 15; i += 4)
+		for (j = 3; j <= 19; j += 4)
+			m++;
+	printf("%d\n", m);
 
 	system("pause");
 	return 0;
